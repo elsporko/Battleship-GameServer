28437a857287a34fb4844682efe8b7153413f10d
John <elsporko@gmail.com>
2019-02-13 21:50:21 -0500

    Rework registration process...incomplete

--- game_server.py
+++ game_server.py
@@ -1,6 +1,6 @@
 import json
 import sys
-sys.path.append('..')
+from Battleship_AWS.AWS.aws import AWS
 
 class Game(object):
     def __init__(self):
@@ -8,13 +8,12 @@ class Game(object):
         self.playerRoster = {} # Placeholder for all players in the game self.playerOrder = [0 for x in range(100)] # Order of player turn
         self.playerOrder = [None for x in range(100)]
 
-game = Game()
 
 def process_message(message):
     payload = json.loads(message)
 
     actions = {
-	    'register': register(payload),
+        'register': register(payload),
     }
 
 def register(payload):
@@ -23,30 +22,30 @@ def register(payload):
     payload=json.loads(msg['Message']) 
     message = {"registration": "FAIL", 'arn': payload['arn']}
     if 'handle' in payload and payload['handle'] not in game.playerRoster:
-	game.playerRoster[payload['handle']]={'handle': payload['handle'], 'arn': payload['arn'], 'order': game.order}
+        game.playerRoster[payload['handle']]={'handle': payload['handle'], 'arn': payload['arn'], 'order': game.order}
 
-	# message returned to caller
-	message = {"registration": "SUCCESS",
-		   "arn": payload["arn"],
-		   "order" : game.order,
-		   "handle" : payload['handle'],
-		   "registered_players" : game.playerRoster
-		  }
-	game.order += 1
+    # message returned to caller
+    message = {"registration": "SUCCESS",
+           "arn": payload["arn"],
+           "order" : game.order,
+           "handle" : payload['handle'],
+           "registered_players" : game.playerRoster
+          }
+    game.order += 1
 
     try:
-	resp = sns_client.publish(TopicArn=payload['arn'], Message=json.dumps(message))
-	print ("Roster: {}\n\n".format(game.playerRoster))
+        resp = sns_client.publish(TopicArn=payload['arn'], Message=json.dumps(message))
+        print ("Roster: {}\n\n".format(game.playerRoster))
     except KeyError:
-	print("payload: ", payload)
-	print("register KeyError")
-	return
+        print("payload: ", payload)
+        print("register KeyError")
+        return
     except TypeError:
-	print("register TypeError")
-	return
+        print("register TypeError")
+        return
     except NotFound:
-	print("register NotFound - Need to do something more than print")
-	return
+        print("register NotFound - Need to do something more than print")
+        return
 
     # next, we delete the message from the queue so no one else will process it again
     ret = sqs_client.delete_message(QueueUrl=queue.url, ReceiptHandle=receipthandle)
@@ -54,12 +53,21 @@ def register(payload):
     return
 
 count=1
+game = Game()
+aws = AWS()
+
+# Set up the queue and topic for registration
+aws.create_queue('Battleship')
+aws.create_topic('Registration')
+aws.add_policy()
+aws.subscribe_to_topic()
+
 while True:
-    messages = sqs_client.receive_message(QueueUrl=queue.url,MaxNumberOfMessages=1, WaitTimeSeconds=20, VisibilityTimeout=3)
+    messages = aws.receive_message()
     print ("received message({}): {}".format(count, messages))
     count = count + 1
     if 'Messages' in messages: # when the queue is exhausted, the response dict contains no 'Messages' key
-	for message in messages['Messages']: # 'Messages' is a list
-	    # process the messages
-	    process_message(json.dumps(message))
+        for message in messages['Messages']: # 'Messages' is a list
+            # process the messages
+            process_message(json.dumps(message))
 
---
+++ tests/json/README.txt
@@ -0,0 +1,3 @@
+This directory contains json files used to manually test parts of the application.
+    register_player.json - Message sent to registration service by a player to
+        register for the game
